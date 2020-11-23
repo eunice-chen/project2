f7c0485cdb87bce2ebe9907ae02e19907ba83f7e
eunice-chen <euniceyiai.eca@gmail.com>
2020-11-24 00:52:48 +0800

    final version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <fstream>
-#define Q_SIZE 1000009
 #include <list>
 #include <vector>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <climits>
+#include <string>
 
 using namespace std;
 
@@ -23,14 +27,34 @@ enum CellType
     charger
 };
 
+enum PathFindingState
+{
+	START,
+	TARGET,
+	// return shortest path
+	PATH,
+	WALL,
+	// done picking and not target/start
+	DONE,
+	// current picking box
+	NOW,
+	BLANK
+};
+
+
 //chech the cell
 class Cell
 {
     public:
     CellType state;
-    //bool isDirty;
-    //bool isBump;
+	Coordinate _position;
+    // path finding
+    PathFindingState _pathState;
+    Cell* parent;
+    int distance;
+	bool isPicked;
 };
+
 
 class FloorMap
 {
@@ -38,11 +62,8 @@ public:
     FloorMap(int row=0, int col=0, int battery = 0);
     Cell **floor;
     int trow, tcol;
-    Coordinate batteryStationCoordinate;
+    Cell* batteryStationCell;
     void printFloor();
-    std::list<Coordinate*> remaining;
-    void RemoveFromRemaining(Coordinate* c);
-    void bfs();
 };
 
 FloorMap::FloorMap(int row, int col, int battery)
@@ -52,7 +73,7 @@ FloorMap::FloorMap(int row, int col, int battery)
     floor = new Cell*[trow];
     for(int i=0; i<trow; i++)
     {
-        floor[i] = new Cell [tcol];
+        floor[i] = new Cell[tcol];
     }
     for(int i=0; i<trow; i++)
     {
@@ -75,13 +96,6 @@ void FloorMap::printFloor()
     }
 }
 
-void FloorMap::RemoveFromRemaining(Coordinate* c)
-{
-    remaining.remove(c);
-}
-
-void
-
 class Robot
 {
 public:
@@ -89,11 +103,35 @@ public:
     int maxBattery, curBattery;
     Coordinate currentPosition;
     vector<Coordinate>StepHistory;
-    void SpiralMove(FloorMap &floor); //移動
-    void MoveToTarget(Coordinate c); //特定的位子
+    list<Cell*> remainingCells;
+    void SpiralMove(FloorMap* floor); //移動
+    void MoveToTarget(Cell* c, FloorMap* floor); //特定的位子
     bool NoBattery();
     void StepRecord(); //記錄走到的位子
+
+    // path finding
+    // to check and run box with current distance
+	// manually initialize and deinitialize
+	static int currentDistance;
+	// run the algorithm based on one box instead of whole board
+	Cell* currentCell;
+	// use it to check all the available boxes' distance
+	std::list<Cell*> children;
+    // function to point currentBox to null
+	void ResetCurrentCell();
+    // restart find path algorithmn if target is found
+    void RestartFindPath(FloorMap* map);
+    void SetSurroundingCell(FloorMap* map);
+    void FindPath(Cell* c, FloorMap* fm);
+
+    stack<Cell*> path; //路径
+
+    bool foundPath;
+
+    void CleanUpRemaining();
 };
+
+int Robot::currentDistance;
 
 Robot::Robot(int battery)
 {
@@ -101,122 +139,363 @@ Robot::Robot(int battery)
     curBattery = battery;
 }
 
-void Robot::SpiralMove(FloorMap &f)
+void Robot::SpiralMove(FloorMap* f)
 {
     //check currentPosition's left cell's state
-    if(f.floor[currentPosition.x][currentPosition.y-1].state == dirty)
+    if(currentPosition.y-1>=0 && f->floor[currentPosition.x][currentPosition.y-1].state == dirty)
     {
-        f.floor[currentPosition.x][currentPosition.y-1].state == clean;  //claer
+        f->floor[currentPosition.x][currentPosition.y-1].state = clean;  //claer
         currentPosition.y = currentPosition.y-1;    //update new position
         StepRecord(); //record step
-        f.RemoveFromRemaining(&currentPosition);
     }
     //check currentPosition's upper cell's state
-    else if(f.floor[currentPosition.x-1][currentPosition.y].state == dirty)
+    else if(currentPosition.x-1 >=0 && f->floor[currentPosition.x-1][currentPosition.y].state == dirty)
     {
-        f.floor[currentPosition.x-1][currentPosition.y].state == clean;  //claer
+        f->floor[currentPosition.x-1][currentPosition.y].state = clean;  //claer
         currentPosition.x = currentPosition.x-1;    //update new position
         StepRecord(); //record step
-        f.RemoveFromRemaining(&currentPosition);
     }
     //check currentPosition's right cell's state
-    else if(f.floor[currentPosition.x][currentPosition.y+1].state == dirty)
+    else if(currentPosition.y+1 < f->tcol && f->floor[currentPosition.x][currentPosition.y+1].state == dirty)
     {
-        f.floor[currentPosition.x][currentPosition.y+1].state == clean;  //claer
+        f->floor[currentPosition.x][currentPosition.y+1].state = clean;  //claer
         currentPosition.y = currentPosition.y+1;    //update new position
         StepRecord(); //record step
-        f.RemoveFromRemaining(&currentPosition);
     }
     //check currentPosition's bottom cell's state
-    else if(f.floor[currentPosition.x+1][currentPosition.y].state == dirty)
+    else if(currentPosition.x+1 < f->trow && f->floor[currentPosition.x+1][currentPosition.y].state == dirty)
     {
-        f.floor[currentPosition.x+1][currentPosition.y].state == clean;  //claer
+        f->floor[currentPosition.x+1][currentPosition.y].state = clean;  //claer
         currentPosition.x = currentPosition.x+1;    //update new position
         StepRecord(); //record step
-        f.RemoveFromRemaining(&currentPosition);
     }
-    /*else
+    else //四周都清理过了，但地图某处还肮脏
     {
-        if(f.remaining.empty() == false)
+        if(remainingCells.empty() == false)
         {
-            MoveToTarget();
+            Cell* newCell = remainingCells.front(); //取得list的第一Cell
+            remainingCells.pop_front(); //清除得到的Cell
+            MoveToTarget(&f->floor[newCell->_position.x][newCell->_position.y], f); // pass in得到的cell节点
         }
-    }*/
+    }
+    CleanUpRemaining();
+}
+
+void Robot::StepRecord()
+{
+    curBattery -= 1;
+    StepHistory.push_back(currentPosition);
 }
 
 bool Robot::NoBattery()
 {
-    if(curBattery == maxBattery/2)
-    {
-        return true;
-    }
-    return false;
+    if(curBattery == maxBattery /2 )
+	{
+		return true;
+	}
+	return false;
 }
 
-//push the coordinate to stepHistory
-void Robot::StepRecord()
+void Robot::ResetCurrentCell()
 {
-    StepHistory.push_back(currentPosition);
+    currentCell = NULL;
+	currentDistance = 0;
+	children.clear();
 }
 
-int main()
+void Robot::RestartFindPath(FloorMap* map)
 {
-
-    int trow, tcol, battery;
-    ifstream inFile("floor3.data");
-    ofstream outFile;
-
-    inFile.is_open();
-        inFile >> trow >> tcol >> battery;
-        FloorMap m(trow, tcol, battery);
-        Robot r(battery);
-        char temp;
-        for(int i=0; i<trow; i++)
-        {
-            for(int j=0; j<tcol; j++)
+    for(int i=0; i<map->trow; i++)
+	{
+		for(int j=0; j<map->tcol; j++)
+		{
+            if(map->floor[i][j].state == wall)
             {
-                inFile >> temp;
-                if(temp == '0')
-                {
-                    Coordinate c;
-                    c.x = i;
-                    c.y = j;
-                    m.remaining.push_back(&c); //push remaining to Coordinate
-                    m.floor[i][j].state = dirty;
-
-                }
-                else if(temp == '1')
-                {
-                    m.floor[i][j].state = wall;
-                }
-                else if(temp == 'R')
-                {
-                    Coordinate c;
-                    c.x = i;
-                    c.y = j;
-                    r.currentPosition = c;
-                    m.floor[i][j].state = charger;
-                }
-            }
-        }
-        /*while(m.remaining.size()>0)
-        {
-            if(r.NoBattery())
-            {
-                r.MoveToTarget(m.batteryStationCoordinate)
+                map->floor[i][j]._pathState = WALL;
             }
             else
             {
-                if(r.canMoveSpiral)
-                {
-                    r.SpiralMove();
-                }
-                else
-                {
-                    r.MoveToTarget()
-                }
+                map->floor[i][j]._pathState = BLANK;
             }
-        }*/
-        m.printFloor();
+
+            map->floor[i][j].distance = INT_MAX;
+            map->floor[i][j].parent = NULL;
+			map->floor[i][j].isPicked = false;
+		}
+	}
+
+    foundPath = false;
+
+	ResetCurrentCell();
+}
+
+// 把周围的cell加入openList
+void Robot::SetSurroundingCell(FloorMap* map)
+{
+    // use own distance + 1
+	int nextdistance = currentCell->distance + 1;
+    // check if the next cell to set up is at the edge
+    // check if the next cell is a blank cell
+    // check if the next cell distance is INT_MAX
+    // bottom
+    if(currentCell->_position.x + 1 < map->trow &&
+       (map->floor[currentCell->_position.x + 1][currentCell->_position.y]._pathState == BLANK || map->floor[currentCell->_position.x + 1][currentCell->_position.y]._pathState == TARGET )
+        && map->floor[currentCell->_position.x + 1][currentCell->_position.y].distance == INT_MAX)
+    {
+        // set its distance to currentDistance + 1
+        map->floor[currentCell->_position.x+1][currentCell->_position.y].distance = nextdistance;
+        // set its parent
+        map->floor[currentCell->_position.x+1][currentCell->_position.y].parent = currentCell;
+        // add to children
+        children.push_back(&map->floor[currentCell->_position.x+1][currentCell->_position.y]);
+
+    }
+
+    //top
+    if(currentCell->_position.x - 1 >= 0 &&
+       (map->floor[currentCell->_position.x - 1 ][currentCell->_position.y]._pathState == BLANK || map->floor[currentCell->_position.x - 1 ][currentCell->_position.y]._pathState == TARGET )
+        && map->floor[currentCell->_position.x - 1 ][currentCell->_position.y].distance == INT_MAX)
+    {
+        // set its distance to currentDistance + 1
+        map->floor[currentCell->_position.x-1][currentCell->_position.y].distance = nextdistance;
+        // set its parent
+        map->floor[currentCell->_position.x-1][currentCell->_position.y].parent = currentCell;
+        // add to children
+        children.push_back(&map->floor[currentCell->_position.x-1][currentCell->_position.y]);
+
+    }
+
+    //right
+    if(currentCell->_position.y + 1 < map->tcol &&
+    (map->floor[currentCell->_position.x][currentCell->_position.y + 1 ]._pathState == BLANK ||
+    map->floor[currentCell->_position.x][currentCell->_position.y + 1 ]._pathState == TARGET ) &&
+    map->floor[currentCell->_position.x][currentCell->_position.y + 1 ].distance == INT_MAX)
+    {
+        // set its distance to currentDistance + 1
+        map->floor[currentCell->_position.x][currentCell->_position.y+1].distance = nextdistance;
+        // set its parent
+        map->floor[currentCell->_position.x][currentCell->_position.y+1].parent = currentCell;
+        // add to children
+        children.push_back(&map->floor[currentCell->_position.x][currentCell->_position.y+1]);
+
+    }
+
+    //left
+    if(currentCell->_position.y - 1 >= 0 &&
+    (map->floor[currentCell->_position.x][currentCell->_position.y - 1 ]._pathState == BLANK ||
+    map->floor[currentCell->_position.x][currentCell->_position.y - 1 ]._pathState == TARGET ) &&
+    map->floor[currentCell->_position.x][currentCell->_position.y - 1 ].distance == INT_MAX)
+    {
+        // set its distance to currentDistance + 1
+        map->floor[currentCell->_position.x][currentCell->_position.y-1].distance = nextdistance;
+        // set its parent
+        map->floor[currentCell->_position.x][currentCell->_position.y-1].parent = currentCell;
+        // add to children
+        children.push_back(&map->floor[currentCell->_position.x][currentCell->_position.y-1]);
+
+    }
+}
+
+void Robot::FindPath(Cell* c, FloorMap* fm)
+{
+    // 设置终点
+    fm->floor[c->_position.x][c->_position.y]._pathState = TARGET;
+
+    // start point, when there is no children
+    if (currentCell == NULL)
+    {
+        currentCell = &fm->floor[currentPosition.x][currentPosition.y];
+        currentCell->_pathState = START;
+        currentCell->distance = 0;
+        SetSurroundingCell(fm);
+        // set current distance to 1 manually
+        currentDistance = 1;
+    }
+    // run normal current distance after diagonal
+    for (std::list<Cell*>::iterator i = children.begin(); i != children.end(); i++)
+    {
+
+        if ((*i)->distance == currentDistance && (*i)->isPicked == false)
+        {
+            // pick the children as current cell
+            currentCell = (*i);
+
+            // change its isPicked to true
+            currentCell->isPicked = true;
+        //    cout << "Current Path State: " << currentCell->_pathState<<endl;
+
+            // check if it is target or blank, if target, end algorithm and change all parents (exclude START) state to PATH
+            if (currentCell->_pathState == TARGET)
+            {
+                // do...while (currentCell->state != currentPosition)
+                do
+                {
+                    path.push(currentCell);
+                    // set current box as the parent
+                    currentCell = currentCell->parent;
+                    // set currentBox state to PATH, if it is not START
+                    if (currentCell->_pathState != START)
+                    {
+                        currentCell->_pathState = PATH;
+                    }
+                } while (currentCell->_pathState != START);
+                // end algorithm
+
+                // stop finding path
+                foundPath = true;
+                return;
+            }
+            // if blank
+            else if (currentCell->_pathState == BLANK)
+            {
+                // set the children surrounding cell
+                SetSurroundingCell(fm);
+                // set its state as DONE
+                currentCell->_pathState = DONE;
+            }
+        }
+    }
+
+    // erase those that is already picked and push into children list
+    for (std::list<Cell*>::iterator l = children.begin(); l != children.end(); l++)
+    {
+        if ((*l)->distance < currentDistance && (*l)->isPicked == true)
+        {
+
+            // erase l from children
+            l = children.erase(l);
+        }
+    }
+
+    // after done with all children (matches with current distance)
+    // increase current distance by one
+    // increment = 1
+    currentDistance += 1;
+
+}
+
+void Robot::MoveToTarget(Cell* c, FloorMap* fm)
+{
+    while(!path.empty())
+    {
+        path.pop();//清空佇列
+    }
+
+    RestartFindPath(fm);
+
+    foundPath = false;
+
+    do
+    {
+        FindPath(c, fm);
+
+    } while (!foundPath);
+    //path储存着从现在地到目的地的coordinates
+    while(!path.empty()) //当q还没被清空，继续行走
+    {
+        if(NoBattery() && c->state != charger) //行动前要确认还有电量，没电池就去充电
+        {
+            //其余的也不走了
+            return;
+        }
+
+        Cell* nextCell = path.top();//从path取出第一节点
+        path.pop(); //清理
+        fm->floor[nextCell->_position.x][nextCell->_position.y].state = clean;  //还没清理的话，就顺便清理了
+        currentPosition = nextCell->_position; //update new position, 现在开始向目的地行走了
+        StepRecord(); //record step，记录节点，之后output
+        //如果在remaining list内，顺便清理了也就要删除
+
+    }
+    CleanUpRemaining();
+    //到达目的地
+}
+
+bool IsCleaned(Cell* c){return (c->state==clean);}
+
+void Robot::CleanUpRemaining()
+{
+    remainingCells.remove_if(IsCleaned);
+}
+
+int main(int argc, char * argv[])
+{
+
+    int trow, tcol, battery;
+    ifstream inFile(argv[1]);
+    ofstream outFile;
+
+    inFile.is_open();
+    inFile >> trow >> tcol >> battery;
+    FloorMap* m = new FloorMap(trow, tcol, battery);
+    Robot* r = new Robot(battery);
+    char temp;
+    for(int i=0; i<trow; i++)
+    {
+        for(int j=0; j<tcol; j++)
+        {
+            inFile >> temp;
+
+            Coordinate c;
+            c.x = i;
+            c.y = j;
+            m->floor[i][j]._position = c; // let cell hold its own coordinate
+            if(temp == '0')
+            {
+                //push remaining to Coordinate
+                m->floor[i][j].state = dirty;
+                r->remainingCells.push_back(&m->floor[i][j]);
+
+            }
+            else if(temp == '1')
+            {
+                m->floor[i][j].state = wall;
+            }
+            else if(temp == 'R')
+            {
+                r->currentPosition = c;
+                r->StepHistory.push_back(c);
+                m->floor[i][j].state = charger;
+                m->batteryStationCell = &m->floor[i][j];
+            }
+        }
+    }
+    inFile.close();
+
+    while(r->remainingCells.size() > 0)
+    {
+        if(r->NoBattery()) //没电池就去充电
+        {
+            r->MoveToTarget(m->batteryStationCell, m);
+        }
+        else
+        {
+            //从外围慢慢进入中心
+            r->SpiralMove(m);
+        }
+    }
+
+    if(r->currentPosition.x != m->batteryStationCell->_position.x && r->currentPosition.y != m->batteryStationCell->_position.y)
+    {
+        r->MoveToTarget(m->batteryStationCell, m);
+    }
+
+    outFile.open("final.path");
+    {
+        if(outFile.is_open())
+        {
+            cout << r->StepHistory.size() << endl;
+            while(r->StepHistory.size()>0)
+            {
+                cout  << r->StepHistory.front().x << "   " << r->StepHistory.front().y << endl;
+                r->StepHistory.erase(r->StepHistory.begin());
+            }
+        }
+        outFile.close();
+    }
+    // release pointers
+    delete r;
+    delete m;
+
     return 0;
 }
